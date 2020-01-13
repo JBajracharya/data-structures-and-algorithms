07d3023691145fb53fad3c85c7841d287333e9f0
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2020-01-13 15:09:47 -0800

    removed large files

--- code401challenges/src/main/java/code401challenges/linkedList/LinkedList.java
+++ code401challenges/src/main/java/code401challenges/linkedList/LinkedList.java
@@ -1,4 +1,47 @@
 package code401challenges.linkedList;
 
 public class LinkedList {
+    Node head;
+
+    public void insert( int value) {
+        Node node = new Node();
+        node.value = value;
+        node.next = null;
+
+        if(head == null) {
+            head = node;
+        } else {
+            Node currentNode = head;
+
+            while(currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+    }
+
+    public boolean includes(int value) {
+        Node currentNode = head;
+        while(currentNode.next != null) {
+            if(currentNode.value == value) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    public String PrintValues() {
+        Node currentNode = head;
+        String listValues = "";
+        while (currentNode.next != null) {
+            listValues += "{" + currentNode.value + "}" + "->";
+            currentNode = currentNode.next;
+        }
+        int lastValue = currentNode.value;
+        currentNode.next = head;
+        System.out.println(listValues + "{" + lastValue + "}" + "->" + "NULL");
+
+        return listValues + "{" + lastValue + "}" + "->" + "NULL";
+    }
 }
--- code401challenges/src/main/java/code401challenges/linkedList/Node.java
+++ code401challenges/src/main/java/code401challenges/linkedList/Node.java
@@ -1,4 +1,6 @@
 package code401challenges.linkedList;
 
 public class Node {
+    int value;
+    Node next;
 }
