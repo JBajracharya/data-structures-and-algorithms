b473231af8dac16d45154793d68830645f6ce5b4
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2020-01-28 00:17:32 -0800

    added all search methods :

--- code401challenges/src/main/java/code401challenges/tree/BinarySearchTree.java
+++ code401challenges/src/main/java/code401challenges/tree/BinarySearchTree.java
@@ -1,24 +1,46 @@
 package code401challenges.tree;
 
-public class BinarySearchTree {
-    Node root;
+import java.util.ArrayList;
 
-    public void add(int value){
-        root = insert(root, value);
+public class BinarySearchTree{
+    ArrayList<Integer> list = new ArrayList<>();
+
+    public ArrayList<Integer> preOrder(Node node) {
+        if(node != null) {
+            list.add(node.data);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+        return list;
     }
 
-    public Node insert(Node node, int val) {
-        if(node == null) {
-            Node newNode = new Node(val);
-            return newNode;
+    public ArrayList<Integer> inOrder(Node node) {
+        if(node != null) {
+            preOrder(node.left);
+            list.add(node.data);
+            preOrder(node.right);
         }
-        if(val < node.data) {
-            node.left = insert(node.left, val);
-        } else if(val > node.data) {
-            node.right = insert(node.right, val);
-        }
-        return node;
+        return list;
     }
 
+    public ArrayList<Integer> postOrder(Node node) {
+        if(node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            list.add(node.data);
+        }
+        return list;
+    }
 
+    public boolean contains( Node root, int value) {
+        list =  postOrder(root);
+        for (int val: list) {
+            if(val == value) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 }
