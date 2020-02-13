cdfe47643f0c80d063c77a47b1794dded197593c
jbajracharya <jitendra.bajracharya01@outlook.com>
2020-02-13 11:42:46 -0800

    added merge sort method

---
+++ code401challenges/src/test/java/code401challenges/sort/MergeSortTest.java
@@ -0,0 +1,34 @@
+package code401challenges.sort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MergeSortTest {
+
+    @Test
+    public void testMergeSort() {
+        int[] arr = {8,4,23,42,16,15, 12};
+        MergeSort.mergeSort(arr);
+        int[] expected = {4, 8, 12, 15, 16, 23, 42};
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void merge() {
+        int[] left = {5, 3};
+        int[] right = {10, 1};
+        int[] arr = {1,1, 2, 3};
+        
+        MergeSort.merge(left, right, arr);
+        int[] expected = {5, 3, 10, 1};
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public  void testEmptyArray() {
+        int[] emptyArray = {};
+        MergeSort.mergeSort(emptyArray);
+        String expected = "Cannot Sort! Array is empty";
+    }
+}
